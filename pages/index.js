7915891edf82fd6cefc1ae9ffa661b18533d8954
Kumari-pratigya <pratigya.sharma101@gmail.com>
2024-03-31 17:22:34 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Script from "next/script";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Description from "./component/description/Description";
