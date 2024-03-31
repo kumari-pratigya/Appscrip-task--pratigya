f64759b82cb95a491169e733a85815890b30214c
Kumari-pratigya <pratigya.sharma101@gmail.com>
2024-03-31 17:29:56 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -23,11 +23,11 @@ export default function Home() {
 <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <Link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></Link>
       </Head>
-      <Script
+      <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"
-      ></Script>
+      ></script>
 
     <Description/>
     <ProductBar/>
