16c6e57107c34c83b9a393f17d3807f48c7ebfba
Kumari-pratigya <pratigya.sharma101@gmail.com>
2024-04-03 12:36:16 +0530

    last commit

--- component/description/Description.jsx
+++ component/description/Description.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import style from './Description.module.css'
 const Description = () => {
   return (
-    <>
+    <div className='bg-white'>
       <div className={`mx-auto my-3 my-sm-5 text-break ${style.descriptionContainer}`}>
         <p className={style.heading}>DISCOVER OUR PRODUCTS</p>
         <p className={style.description}>Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus scelerisque. Dolor integer scelerisque nibh amet mi ut elementum dolor.</p>
       </div>
-    </>
+    </div>
   )
 }
 
--- component/productContainer/ProductContainer.jsx
+++ component/productContainer/ProductContainer.jsx
@@ -3,11 +3,13 @@ import Products from '../products/Products'
 import SideBar from '../sideBar/SideBar'
 const ProductContainer = () => {
   return (
-    <div className='row gx-0 mx-0 mx-sm-5 bg-white'>
+    <div className='bg-white'>
+    <div className='row gx-0 mx-0 mx-sm-5 '>
       <div className='col-md-3 ps-md-0 pe-md-3 d-none d-md-block'>
         <SideBar/>
       </div>
       <div className='col-md-9 col-12'><Products/></div>   
+    </div>
     </div>
   )
 }
